c4ce7e3c2e0086ab2c930441ee6796da692e51c7
Yuri <yuridasilvamenezes@gmail.com>
2019-11-22 19:01:24 -0300

    teste

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -1,4 +1,4 @@
-// import { parseISO, addMonths } from 'date-fns';
+import { startOfHour, parseISO, addMonths } from 'date-fns';
 import Enrollment from '../models/Enrollment';
 import Student from '../models/Student';
 import Plan from '../models/Plan';
@@ -27,20 +27,27 @@ class EnrollmenteController {
   }
 
   async store(req, res) {
-    const { student_id, plan_id, start_date, price } = req.body;
+    const { student_id, plan_id, start_date } = req.body;
 
-    // const plan = await Plan.findByPk(plan_id);
+    const plan = await Plan.findByPk(plan_id);
 
-    // const end_date = addMonths(parseISO(start_date), plan.duration);
-    // const final_price = price * plan.duration;
+    const startDate = startOfHour(start_date);
 
-    const createEnrollment = await Enrollment.create({
+    const { duration, price } = plan;
+
+    const priceTotal = price * duration;
+
+    const endDate = addMonths(startDate, duration);
+
+    console.log(startDate);
+    console.log(endDate);
+
+    const enrollment = await Enrollment.create({
       student_id,
       plan_id,
       start_date,
-      price,
-      // end_date,
-      // final_price,
+      end_date: endDate,
+      price: priceTotal,
     });
 
     const email = await Enrollment.findByPk(plan_id, {
@@ -65,7 +72,7 @@ class EnrollmenteController {
       },
     });
 
-    return res.json(createEnrollment);
+    return res.json(enrollment);
   }
 }
 
